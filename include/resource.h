94cb1d20879b92b86d090be38fdc2d542c975137
Grigory Pavlenko <Grigory.PAVLENKO@bachmann.info>
2017-06-22 11:20:56 +0200

    added controls

--- include/resource.h
+++ include/resource.h
@@ -1,10 +1,16 @@
-#define IDI_APPICON                    101
-#define IDR_MAINMENU                   102
-#define IDR_ACCELERATOR                103
-#define IDD_ABOUTDIALOG                104
-#define ID_FILE_EXIT                   40001
-#define ID_HELP_ABOUT                  40002
+#define IDI_APPICON						101
+#define IDR_MAINMENU					102
+#define IDR_ACCELERATOR					103
+#define IDD_ABOUTDIALOG					104
+#define IDD_TOOLBAR						105
+#define ID_FILE_EXIT					40001
+#define ID_HELP_ABOUT					40002
+#define ID_PROPERTIES					40003
+#define IDC_PRESS						40004
+#define IDC_OTHER						40005
+#define IDC_CLOSE_SETT					40006
+#define IDC_BAR1_EN						40007
 
 #ifndef IDC_STATIC
-  #define IDC_STATIC                   -1
+  #define IDC_STATIC					-1
 #endif
