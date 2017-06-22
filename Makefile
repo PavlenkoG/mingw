77f43e717508fc0731deba3f02ce804e91930ba3
Grigory Pavlenko <Grigory.PAVLENKO@bachmann.info>
2017-06-22 08:57:08 +0200

    Add project files to the project

---
+++ Makefile
@@ -0,0 +1,9 @@
+OBJS = obj/winmain.o obj/winmain.exe obj/resource.o obj/callbacks.o
+all: 
+	windres -I.\include -I.\res -i res/resource.rc -o obj/resource.o
+	gcc -I"C:\Users\GRPA\Documents\WORK\C_Projects\mingw\include" -O0 -g -D _WIN32_IE=0x0900 -c src/callbacks.c -o obj/callbacks.o "C:\\MinGW\\lib\\libgdi32.a"
+	gcc -I"C:\Users\GRPA\Documents\WORK\C_Projects\mingw\include" -O0 -g -D _WIN32_IE=0x0900 -c src/winmain.c -o obj/winmain.o
+	gcc obj/resource.o obj/callbacks.o obj/winmain.o -o obj/winmain.exe -g -D _WIN32_IE=0x0900 "C:\\MinGW\\lib\\libgdi32.a" -lcomctl32 -Wl,--subsystem,windows
+
+clean: ${OBJS}
+	rm  $^
---
+++ src/callbacks.c
@@ -0,0 +1,130 @@
+#include "callbacks.h"
+#include <math.h>
+#include "resource.h"
+const int R = 100;
+// Window procedure for our main window.
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HINSTANCE hInstance;
+	PAINTSTRUCT ps;
+		HDC hdc;
+		int a, b, x, y;
+		static int sx,sy;
+		switch (msg)
+		{
+			case WM_SIZE:
+				sx = LOWORD(lParam);
+				sy = HIWORD(lParam);
+				InvalidateRect(hWnd, NULL, TRUE);
+				break;
+			case WM_PAINT:
+					hdc = BeginPaint (hWnd, &ps);
+					a = sx/2;
+					b = sy/2;
+					MoveToEx(hdc,0,b,NULL);
+					LineTo (hdc,sx,b);
+					MoveToEx(hdc,a,0,NULL);
+					LineTo(hdc,a,sy);
+					MoveToEx(hdc,a,b,NULL);
+					for (float angle = 0.0; angle < 2*M_PI; angle += 0.1)
+					{
+						x = a + R*(1 - cos(angle))*cos(angle);
+						y = b - R*(1 - cos(angle))*sin(angle);
+						LineTo(hdc, x, y);
+					}
+
+					//for (x=0; x<sx; x+=sx/10)
+					//{
+					//	MoveToEx(hdc,x,0,NULL);
+					//	LineTo(hdc,x,sy);
+					//}
+					//for (y = 0; y <sy; y+= sy/10)
+					//{
+					//	MoveToEx(hdc,0,y,NULL);
+					//	LineTo(hdc,sx,y);
+					//}
+					//Rectangle (hdc, sx/4, sy/4, sx*3/4, sy*3/4);
+					//Ellipse (hdc, sx/3, sy/4, sx*2/3, sy*3/4);
+					EndPaint (hWnd,&ps);
+					break;
+				case WM_COMMAND:
+				{
+					switch (LOWORD(wParam))
+					{
+						case ID_HELP_ABOUT:
+						{
+							DialogBox(hInstance, MAKEINTRESOURCE(IDD_ABOUTDIALOG), hWnd, &AboutDialogProc);
+							return 0;
+						}
+
+						case ID_FILE_EXIT:
+						{
+							DestroyWindow(hWnd);
+							return 0;
+						}
+					}
+					break;
+				}
+
+				case WM_GETMINMAXINFO:
+				{
+					MINMAXINFO *minMax = (MINMAXINFO*) lParam;
+					minMax->ptMinTrackSize.x = 220;
+					minMax->ptMinTrackSize.y = 110;
+					return 0;
+				}
+				case WM_SYSCOMMAND:
+				{
+					switch (LOWORD(wParam))
+					{
+						case ID_HELP_ABOUT:
+						{
+							DialogBox(hInstance, MAKEINTRESOURCE(IDD_ABOUTDIALOG), hWnd, &AboutDialogProc);
+							return 0;
+						}
+					}
+					break;
+				}
+
+				case WM_CREATE:
+				{
+					hInstance = ((LPCREATESTRUCT) lParam)->hInstance;
+					return 0;
+				}
+
+				case WM_DESTROY:
+				{
+					PostQuitMessage(0);
+					return 0;
+				}
+		}
+
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+
+// Dialog procedure for our "about" dialog.
+INT_PTR CALLBACK AboutDialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		case WM_COMMAND:
+		{
+			switch (LOWORD(wParam))
+			{
+				case IDOK:
+				case IDCANCEL:
+				{
+					EndDialog(hwndDlg, (INT_PTR) LOWORD(wParam));
+					return (INT_PTR) TRUE;
+				}
+			}
+			break;
+		}
+
+		case WM_INITDIALOG:
+		return (INT_PTR) TRUE;
+	}
+
+	return (INT_PTR) FALSE;
+}
