ea0337848818f63391d9e2356cabdf7a694d9ca4
Grigory Pavlenko <Grigory.PAVLENKO@bachmann.info>
2017-06-22 11:19:39 +0200

    added ToolDlgProc

--- include/callbacks.h
+++ include/callbacks.h
@@ -4,9 +4,11 @@
 #include <windows.h>
 
 // Window procedure for our main window.
-LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK MainWndProc(HWND hWnd,  UINT msg, WPARAM wParam, LPARAM lParam);
 
 // Dialog procedure for our "about" dialog.
 INT_PTR CALLBACK AboutDialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+BOOL CALLBACK ToolDlgProc(HWND hWnd, UINT Message, WPARAM wParam, LPARAM lParam);
 
 #endif
