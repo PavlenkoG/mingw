60e6c056c37a104b434e04dd9bbbd71e29bbe912
Grigory Pavlenko <Grigory.PAVLENKO@bachmann.info>
2017-06-28 09:28:12 +0200

    setup dialog window updated

--- include/resource.h
+++ include/resource.h
@@ -3,6 +3,13 @@
 #define IDR_ACCELERATOR					103
 #define IDD_ABOUTDIALOG					104
 #define IDD_TOOLBAR						105
+#define IDC_MAIN_TOOL					200
+#define IDC_MAIN_EDIT					201
+#define IDC_NEW							30000
+#define IDC_OPEN						30001
+#define IDC_SAVE						30002
+#define ID_MDI_FIRSTCHILD				106
+#define IDC_MAIN_MDI					107
 #define ID_FILE_EXIT					40001
 #define ID_HELP_ABOUT					40002
 #define ID_PROPERTIES					40003
@@ -15,8 +22,23 @@
 #define IDC_BAR4_EN						40010
 #define IDC_BAR5_EN						40011
 #define IDC_BAR6_EN						40012
-#define IDC_BAR1_TXT					40013
-
+#define IDC_INT_ENA						40013
+#define IDC_WR32_ENA					40014
+#define IDC_WR16_ENA					40015
+#define IDC_WR8_ENA					    40016
+#define IDC_WRBR_ENA					40017
+#define IDC_CH1_ENA  					40018
+#define IDC_CH2_ENA  					40019
+#define IDC_BAR1_TXT					40020
+#define IDC_BAR2_TXT					40021
+#define IDC_BAR3_TXT					40022
+#define IDC_BAR4_TXT					40023
+#define IDC_BAR5_TXT					40024
+#define IDC_BAR6_TXT					40025
+#define IDC_LSB_AR_TXT					40026
+#define IDC_MSB_AR_TXT					40027
+#define IDC_MSG_TXT						40028
+#define IDC_VECT_QNTY_TXT				40029
 #ifndef IDC_STATIC
   #define IDC_STATIC					-1
 #endif
